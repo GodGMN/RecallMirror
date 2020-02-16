41cd74bbd9a92b6b2745dcbcbbe7080bd6e5f5bb
God <sergiogmn@gmail.com>
2020-02-16 10:11:57 +0100

    Updated source

---
+++ src/main/java/com/godgmn/recallmirror/init/ModBlocks.java
@@ -0,0 +1,11 @@
+package com.godgmn.recallmirror.init;
+
+import com.godgmn.recallmirror.RecallMirror;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+public class ModBlocks {
+	@ObjectHolder("recallmirror:butt_block")
+    public static final Block BUTT_BLOCK = null;
+}
